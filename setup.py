b29f3f72d360091f4668ee072aa7443de2359079
goujilinouhaila-coder <goujilinouhaila@gmail.com>
2021-03-17 13:29:29 +0100

    Setup_tools

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup
 from biketrauma import __version__ as current_version
 
 setup(
-  name='biketrauma',
+  name='biketrauma_ng',
   version=current_version,
   description='Visualization of a bicycle accident db',
-  url='http://github.com/xxxxxxxxxxx.git',
-  author='xxxxxxxxxxx',
-  author_email='xxxxxxxxxx@xxxxxxxxxxxxx.xxx',
+  url='https://github.com/goujilinouhaila-coder/Bike_trauma.git',
+  author='Goujili Nouhaila',
+  author_email='goujilinouhaila@gmail.com',
   license='MIT',
   packages=['biketrauma','biketrauma.io', 'biketrauma.preprocess', 'biketrauma.vis'],
   zip_safe=False
